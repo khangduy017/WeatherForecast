c3e25eda6089d0a527a29b47a71c45d66ea54f92
khangduy017 <khangduy017@gmail.com>
2024-05-31 05:08:14 +0700

    [Fix] Config vercel

--- frontend/lib/services/endpoint.dart
+++ frontend/lib/services/endpoint.dart
@@ -1,6 +1,6 @@
-const String baseURL = "http://127.0.0.1:3000/weatherForecast/api/v1";
+// const String baseURL = "http://127.0.0.1:3000/weatherForecast/api/v1";
 // const String baseURL =
 //     "https://weatherforecast-d1lk.onrender.com/weatherForecast/api/v1";
 
-// const String baseURL =
-//     "https://weatherforecast-20.onrender.com/weatherForecast/api/v1";
+const String baseURL =
+    "https://weatherforecast-20.onrender.com/weatherForecast/api/v1";
